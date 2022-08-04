dc2bfce4932c531878a112f4ec8cfa2a84cc8e4e
nourana23-meet <109900008+nourana23-meet@users.noreply.github.com>
2022-08-04 10:31:40 +0300

    finished, but i will try to add more...

--- app.py
+++ app.py
@@ -29,16 +29,18 @@ db = firebase.database()
 app = Flask(__name__,template_folder='templates',static_folder='static')
 app.config['SECRET_KEY'] = '252548527'
 
-username = "1"
-password = "1"
 
 @app.route('/',methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
-		username = request.form['username']
+		email = request.form['email']
 		password = request.form['password']
+		Name = request.form['name']
+		Bio = request.form['bio']
 		try:
 			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+			user = {"name": Name, "bio": Bio}
+			db.child("usera").child(login_session['user']['localId']).set(user)
 			return redirect(url_for('Home'))
 		except:
 			return render_template('signin.html')
@@ -46,17 +48,21 @@ def login():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    error = ""
-    if request.method == 'POST':
-        email = request.form['username']
-        password = request.form['password']
-        try:
-            login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('Home'))
-        except:
-            error = "Authentication failed"
-    return render_template("signup.html")
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']			
+		try:
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
+			return redirect(url_for('Home'))
+		except:
+			error = "Authentication failed"
+	return render_template("signup.html")
 
+
+@app.route("/product")
+def Product():
+	return render_template("product.html")
 
 
 @app.route('/home')
